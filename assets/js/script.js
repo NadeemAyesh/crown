f4c65e22086c31afb115eb442af7632f6acb3585
Nadeem Ayesh <35177414+NadeemAyesh@users.noreply.github.com>
2023-04-15 12:52:56 +0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,4 +77,58 @@ $(document).ready(function () {
         });
     }
 
+    var formatForSlider = {
+        from: function (formattedValue) {
+            return Number(formattedValue);
+        },
+        to: function(numericValue) {
+            return Math.round(numericValue);
+        }
+    };
+
+    if ($('#formatting-slider').length != 0) {
+        var formatSlider = document.getElementById('formatting-slider');
+        noUiSlider.create(formatSlider, {
+            // Values are parsed as numbers using the "from" function in "format"
+            start: ['20.0', '80.0'],
+            connect: true,
+            range: {
+                'min': 0,
+                'max': 100
+            },
+            format: formatForSlider,
+            tooltips: {
+                // tooltips are output only, so only a "to" is needed
+                to: function(numericValue) {
+                    return numericValue.toFixed(1);
+                }
+            }
+        });
+
+        formatSlider.noUiSlider.set(['25.666', '57.66']);
+
+        var formatValues = [
+            document.getElementById('formatting-start'),
+            document.getElementById('formatting-end')
+        ];
+
+        formatSlider.noUiSlider.on('update', function (values, handle, unencoded) {
+            // "values" has the "to" function from "format" applied
+            // "unencoded" contains the raw numerical slider values
+            formatValues[handle].innerHTML = values[handle];
+        });
+    }
+
+    $('.show-grid').on('click', function(e) {
+        e.preventDefault();
+        $('.hide-archive').hide();
+        $('.grid-items').fadeIn();
+    })
+
+    $('.show-list').on('click', function(e) {
+        e.preventDefault();
+        $('.hide-archive').hide();
+        $('.list-items').fadeIn();
+    })
+
 });
